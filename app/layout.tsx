6c511d035c182c775ccc83289a06abe36ff51c3c
Adeel Akhani <akhaniadeel@gmail.com>
2025-02-12 02:00:35 -0500

    fixed favicon issue

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,30 +1,35 @@
-import "@/styles/globals.css"
-import { Inter } from "next/font/google"
-import { ThemeProvider } from "next-themes"
-import { Navbar } from "@/components/navbar"
-import type React from "react"
-import { Toaster } from "@/components/ui/toaster"
+import "@/styles/globals.css";
+import { Inter } from "next/font/google";
+import { ThemeProvider } from "next-themes";
+import { Navbar } from "@/components/navbar";
+import type React from "react";
+import { Toaster } from "@/components/ui/toaster";
 
-const inter = Inter({ subsets: ["latin"] })
-
+const inter = Inter({ subsets: ["latin"] });
 
 export default function RootLayout({
   children,
 }: {
-  children: React.ReactNode
+  children: React.ReactNode;
 }) {
   return (
-    <html lang="en" suppressHydrationWarning>
+    <html lang="en">
       <head>
-      <link rel="icon" href="/favicon.ico" sizes="any" />
-    </head>
+        <link rel="icon" href="/favicon.ico" type="image/x-icon" />
+        
+      </head>
       <body className={`${inter.className} bg-black text-white antialiased`}>
-        <ThemeProvider attribute="class" defaultTheme="dark" enableSystem={false} forcedTheme="dark">
+        <ThemeProvider
+          attribute="class"
+          defaultTheme="dark"
+          enableSystem={false}
+          forcedTheme="dark"
+        >
           <Navbar />
           {children}
           <Toaster />
         </ThemeProvider>
       </body>
     </html>
-  )
+  );
 }
